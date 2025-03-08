ef6c8f30ee8fe24bc1abbc50341346d6924f6148
Zain <zainsantos21@gmail.com>
2025-03-08 17:12:47 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+} 
+saturdayFun();
+saturdayFun("bathe my dog");
+
+(function () {
+    console.log("Yet more razzling");
+  });
+
+    function mondayWork(activity = "go to the office") {
+      return `This Monday, I will ${activity}.`;
+    }
+    mondayWork();
+
+    function wrapAdjective(flair = "*") {
+      return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+      };
+    }
+    wrapAdjective("%")("a dedicated programmer");
